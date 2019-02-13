03daa9c961e06aabfb20d0f5167b68feddacf568
kevin_home <kevin_jfzhu@163.com>
2019-02-13 09:23:26 +0800

    revising pipeline

--- tests.py
+++ tests.py
@@ -1,12 +1,6 @@
 from utils.pipeline import *
 
-
-'''
-sh_xiaoqu = get_xiaoqu_list('sh')
-print(sh_xiaoqu)
-'''
-
-
 xiaoqu_url = 'https://sh.lianjia.com/xiaoqu/5011000016470/'
-info = get_xiaoqu_detailed_info(xiaoqu_url)
-print()
+h = request_lianjia_url(xiaoqu_url, lib='bs4')
+
+print(h.prettify())
