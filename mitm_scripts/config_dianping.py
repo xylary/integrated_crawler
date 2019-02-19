b17e2163d12604f85ffc010a5b006c8ede44ea6a
kevin_jfzhu <kevin_jfzhu@163.com>
2019-02-19 18:45:30 +0800

    19-02-18 office: updated mitmdump dianping script

--- mitm_scripts/config_dianping.py
+++ mitm_scripts/config_dianping.py
@@ -1,22 +1,34 @@
 # -*- coding: utf-8 -*-
 
+import mitmproxy.addonmanager
+import mitmproxy.connections
+import mitmproxy.http
+import mitmproxy.log
+import mitmproxy.tcp
+import mitmproxy.websocket
+import mitmproxy.proxy.protocol
 from mitmproxy import ctx
 
 
 def request(flow):
-    if flow.request.method == "GET":
-        # If the decision is done by domain, one could also modify the server address here.
-        # We do it after CONNECT here to have the request data available as well.
-        client_ip = flow.client_conn.address[0]
-        print(client_ip)
-        print('OK')
-    '''    
-        if 'ip.cn' in flow.request.url:
-                ctx.log.info(flow.request.url)
-                proxy = ("localhost", 8080)
-        else:
-                proxy = ("localhost", 3800)
-    # 这里配置二级代理的ip地址和端口
-    if flow.live:
-        flow.live.change_upstream_proxy_server(proxy)
-    '''
+    """修改应答数据"""
+    if 'upstream_proxy=' in flow.request.url:
+        flow.request.url = flow.request.url.replace('baidu', 'qq')
+
+
+def response(flow):
+    """修改应答数据"""
+    if '/js/yoda.' in flow.request.url:
+        # 屏蔽selenium检测
+        for webdriver_key in ['webdriver', '__driver_evaluate', '__webdriver_evaluate', '__selenium_evaluate',
+                              '__fxdriver_evaluate', '__driver_unwrapped', '__webdriver_unwrapped',
+                              '__selenium_unwrapped', '__fxdriver_unwrapped', '_Selenium_IDE_Recorder', '_selenium',
+                              'calledSelenium', '_WEBDRIVER_ELEM_CACHE', 'ChromeDriverw', 'driver-evaluate',
+                              'webdriver-evaluate', 'selenium-evaluate', 'webdriverCommand',
+                              'webdriver-evaluate-response', '__webdriverFunc', '__webdriver_script_fn',
+                              '__$webdriverAsyncExecutor', '__lastWatirAlert', '__lastWatirConfirm',
+                              '__lastWatirPrompt', '$chrome_asyncScriptInfo', '$cdc_asdjflasutopfhvcZLmcfl_']:
+            ctx.log.info('Remove "{}" from {}.'.format(webdriver_key, flow.request.url))
+            flow.response.text = flow.response.text.replace('"{}"'.format(webdriver_key), '"NO-SUCH-ATTR"')
+            flow.response.text = flow.response.text.replace('t.webdriver', 'false')
+            flow.response.text = flow.response.text.replace('ChromeDriver', '')
