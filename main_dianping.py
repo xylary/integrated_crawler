674966d4d025fb73d5115568ce419c3009e14e57
kevin_home <kevin_jfzhu@163.com>
2019-02-26 09:32:26 +0800

    update crawling

--- main_dianping.py
+++ main_dianping.py
@@ -3,6 +3,40 @@
 
 from pipelines.pipeline_dianping import *
 
-search_keyword_in_dianping('至尊堡', start_city_id=91)
+search_keyword_in_dianping('肯德基', start_city_id=105)
 
 # clean_data()
+'''
+奥多姆汉堡 8
+奥克派炸鸡汉堡 7
+澳克士 8
+澳客100炸鸡汉堡 7
+八戒汉堡 11
+堡中堡 10
+伯仕基 10
+超级堡 14
+超级汉堡 15
+
+华克仕炸鸡汉堡 14
+
+鸡蛋汉堡 16
+加利士汉堡 14
+肯得乐17
+
+嘟市基 18
+欢乐堡 18
+客莱茵 18
+常乐基 18
+爱尚客披萨 18
+巴必屋 16
+  
+大麦乐汉堡 17
+当麦基 17
+德尔客
+汉堡王子 14
+汉堡屋 11
+汉堡侠 16
+嗨咪呢汉堡 16
+聚乐港 12
+
+'''
