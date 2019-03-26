f6e28a36e6afbbaaec929961d23fd5e99027f4fc
kevin_home <kevin_jfzhu@163.com>
2019-03-26 08:21:10 +0800

    update xuexi.py

--- xuexi.py
+++ xuexi.py
@@ -1,16 +1,16 @@
 from utils.selenium_webdriver import *
 
 
-look_articles = False
+look_articles = True
 watch_videos = True
 headless_mode = False
 
 
-TIME_TO_WAIT_ARTICLE = 10
-TIME_TO_WAIT_VIDEO = 620
+TIME_TO_WAIT_ARTICLE = 30
+TIME_TO_WAIT_VIDEO = 310
 login_url = 'https://pc.xuexi.cn/points/login.html?ref=https://www.xuexi.cn/'
-huiyi_url = 'https://www.xuexi.cn/89acb6d339cd09d5aaf0c2697b6a3278/9a3668c13f6e303932b5e0e100fc248b.html'
-shipin_url = 'https://www.xuexi.cn/4426aa87b0b64ac671c96379a3a8bd26/db086044562a57b441c24f2af1c8e101.html'
+toutiao_url = 'https://www.xuexi.cn/72ac54163d26d6677a80b8e21a776cfa/9a3668c13f6e303932b5e0e100fc248b.html'
+video_url = 'https://www.xuexi.cn/4426aa87b0b64ac671c96379a3a8bd26/db086044562a57b441c24f2af1c8e101.html'
 
 
 # Login and get cookies
@@ -28,28 +28,42 @@ while True:
 b = init_browser('https://www.baidu.com', headless=headless_mode)
 b.delete_all_cookies()
 for cookie in cookies:
+    print(cookie)
     b.add_cookie(cookie)
 
-b.get('https://www.xuexi.cn/89acb6d339cd09d5aaf0c2697b6a3278/9a3668c13f6e303932b5e0e100fc248b.html')
+b.get(toutiao_url)
 time.sleep(1)
+
+
+print('\nStart time: ', time.ctime())
 
 if look_articles:
     # Look articles
     huiyi_handle = b.current_window_handle
-    huiyi_elems = b.find_elements_by_xpath('//*[@id="Chj9orckh95s00"]/div')
+    huiyi_elems = b.find_elements_by_xpath('//*[@id="C9vbojkfmu8w00"]/div')
 
     for i in range(0, 5):
         huiyi_elems[i].click()
         b.switch_to.window(b.window_handles[-1])
-        print(b.current_url)
+        print('Reading article: ', b.current_url, ', time: ', time.ctime())
         for i in range(5):
             time.sleep(TIME_TO_WAIT_ARTICLE)
             b.execute_script('window.scrollBy(0, 1000)')
         b.close()
         b.switch_to.window(huiyi_handle)
 
+    b.switch_to.window(huiyi_handle)
+    huiyi_elems[5].click()
+    b.switch_to.window(b.window_handles[-1])
+    print('Reading article: ', b.current_url, ', time: ', time.ctime())
+    for i in range(5):
+        time.sleep(TIME_TO_WAIT_ARTICLE)
+        b.execute_script('window.scrollBy(0, 1000)')
+    time.sleep(2200)
+
+
 if watch_videos:
-    b.execute_script("window.open('{}', 'new_window')".format(shipin_url))
+    b.execute_script("window.open('{}', 'new_window')".format(video_url))
     b.switch_to.window(b.window_handles[-1])
     shipin_handle = b.current_window_handle
     shipin_elems = b.find_elements_by_xpath('//*[@id="Cd5zymfz1fzs0"]/div')
@@ -57,21 +71,15 @@ if watch_videos:
     for i in range(0, 6):
         shipin_elems[i].click()
         b.switch_to.window(b.window_handles[-1])
-        print(b.current_url)
+        print('Watching video: ', b.current_url, ', time: ', time.ctime())
         b.execute_script('window.scrollBy(0, 1000)')
         time.sleep(TIME_TO_WAIT_VIDEO)
+        if i == 5:
+            time.sleep(TIME_TO_WAIT_VIDEO * 5)
         b.close()
         b.switch_to.window(shipin_handle)
 
     b.close()
 
-if look_articles:
-    b.switch_to.window(huiyi_handle)
-    huiyi_elems[5].click()
-    b.switch_to.window(b.window_handles[-1])
-    for i in range(5):
-        time.sleep(TIME_TO_WAIT_ARTICLE)
-        b.execute_script('window.scrollBy(0, 1000)')
-    time.sleep(2700)
 
 b.quit()
